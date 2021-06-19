87ed312c661c29edc2dbe2b61209e4c2091ffa4b
unknown <you@example.com>
2021-06-18 18:47:48 -1000

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+// app
+const app = express()
+
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// routes
+app.get('/api', (req, res) => {
+  res.json({ time: Date().toString() })
+})
+
+// port
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
